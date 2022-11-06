24fe0312d8530231dd86a121c792e93dd7cddeb9
ViJ <propellerheadvij@gmail.com>
2022-11-07 00:26:30 +0300

    Canvas test

--- Assets/Plugins/ThirdParty/CircuitJSharpTest/CircuitTest.cs
+++ Assets/Plugins/ThirdParty/CircuitJSharpTest/CircuitTest.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
+using ViJApps.CanvasTexture;
 
 public class CircuitTest : MonoBehaviour
 {
-    private LineRenderer _lineRenderer;
+    [SerializeField] private Renderer _renderer;
 
     private CirSim _cirSim;
     private ResistorElm _resistor;
     private VoltageElm _voltage;
-    
+
+    private CanvasTexture _canvasTexture;
+    private bool _isReady;
+
     // Init Circuit
-    private void Start()
+    private async void Start()
     {
+        await MaterialProvider.Initialization;
+        _isReady = true;
+
+        _canvasTexture = new CanvasTexture();
+        _canvasTexture.Init(256, 128);
+        _canvasTexture.AspectSettings.Aspect = 0.5f;
+        _renderer.material.mainTexture = _canvasTexture.RenderTexture;
+
         _cirSim = new CirSim();
         _cirSim.init();
-        
+
+
         //Create some test elements
         _resistor = new ResistorElm(0, 1);
         _voltage = new VoltageElm(0, 1, WaveForm.WF_AC);
@@ -24,10 +39,35 @@ public class CircuitTest : MonoBehaviour
     }
 
     // Update Circtuit
+    
+    public struct VoltagePoint
+    {
+        public float Time;
+        public float Voltage;
+    }
+
+    private Queue<VoltagePoint> _queue = new Queue<VoltagePoint>();
+    
     private void Update()
     {
+        if (!_isReady)
+            return;
+            
         _cirSim.timeDelta = Time.deltaTime;
         _cirSim.updateCircuit();
-        Debug.Log(_resistor.getVoltageDiff());
+
+        var point = new VoltagePoint()
+        {
+            Time = (float)_cirSim.t,
+            Voltage = (float)_resistor.getVoltageDiff(),
+        };
+        _queue.Enqueue(point);
+        if (_queue.Count > 100)
+            _queue.Dequeue();
+
+        var startFrom = _queue.Peek().Time;
+        _canvasTexture.ClearWithColor(Color.white);
+        _canvasTexture.DrawPolyLine(_queue.Select(x => new float2(x.Time - startFrom, 0.5f + x.Voltage / 10f)).ToList(), 0.05f, Color.red);
+        _canvasTexture.Flush();
     }
 }
