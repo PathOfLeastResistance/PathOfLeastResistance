415e130b94d72f16abf1b2d497dfb0b8927d7405
ViJ <propellerheadvij@gmail.com>
2022-11-26 21:55:12 +0300

    Camera is done

--- Assets/Game/Camera/SimpleCameraInteraction.cs
+++ Assets/Game/Camera/SimpleCameraInteraction.cs
@@ -10,33 +10,47 @@ using Zenject;
 
 namespace Game
 {
+    [Serializable]
+    public struct CameraSettings
+    {
+        public AnimationCurve m_heightCurve;
+        public AnimationCurve m_targetForwardOffsetCurve;
+        public AnimationCurve m_cameraForwardOffsetCurve;
+    }
+
     public class SimpleCameraInteraction : MonoBehaviour
     {
         [Inject] private CameraRaycaster m_cameraRaycaster;
-        
+
         [SerializeField] private Camera m_camera;
-        [SerializeField] private float m_distance = 1f;
-        [SerializeField] private float2 m_MinMaxDistance = new float2(1f, 10f);
+        [SerializeField] private float m_zoom = 1f;
         [SerializeField] private PlanarColliderPositionClamper m_Clamper;
 
         [SerializeField] private CinemachineVirtualCamera m_virtualCamera;
         [SerializeField] private Transform m_TargetTransform;
 
         [SerializeField] private InteractionObject m_InteractionObject;
+        [SerializeField] private CameraSettings m_cameraSettings;
+        [SerializeField] private float m_RotationSensetivity = 1f;
 
         private Plane m_InteractionPlane = new Plane(Vector3.up, Vector3.zero);
         private CinemachineTransposer m_Transposer;
+        private CinemachineComposer m_Composer;
         private Vector3 m_TargetDragStartPoint;
         private Vector3 m_TotalDelta;
         private float m_WheelSensitive = 0.01f;
 
-        public float Distance
+        public float Zoom
         {
-            get => m_distance;
+            get => m_zoom;
             set
             {
-                m_distance = math.clamp(value, m_MinMaxDistance.x, m_MinMaxDistance.y);
-                m_Transposer.m_FollowOffset = new Vector3(0, 0, m_distance);
+                m_zoom = math.clamp(value, 0, 1);
+                var heightOffset = m_cameraSettings.m_heightCurve.Evaluate(m_zoom);
+                var targetForwardOffset = m_cameraSettings.m_targetForwardOffsetCurve.Evaluate(m_zoom);
+                var cameraForwardOffset = m_cameraSettings.m_cameraForwardOffsetCurve.Evaluate(m_zoom);
+                m_Transposer.m_FollowOffset = new Vector3(0, heightOffset, cameraForwardOffset);
+                m_Composer.m_TrackedObjectOffset = new Vector3(0, 0, targetForwardOffset);
             }
         }
 
@@ -49,17 +63,20 @@ namespace Game
         private void Awake()
         {
             InputManager.Instance.RegisterCamera(m_camera);
-            GameExtraInput.Instance.OnWheelEvent += OnWheel;
+            SimpleMouseInput.Instance.OnWheelEvent += OnWheel;
+            SimpleMouseInput.Instance.SubscribeLeftButtonDrag(OnLeftDragStart, OnLeftDragPerformed, OnLeftDragEnd);
+            SimpleMouseInput.Instance.SubscribeRightButtonDrag(OnRightDragStart, OnRightDragPerformed, OnRightDragEnd);
+
             m_Transposer = m_virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
-            m_InteractionObject.SubscribePointerDragEvent(OnPointerDragStart, OnPointerDrag, OnPointerDragEnd);
+            m_Composer = m_virtualCamera.GetCinemachineComponent<CinemachineComposer>();
 
             TargetPosition = Vector3.zero;
-            Distance = 1;
+            Zoom = 1;
         }
 
-        private void OnPointerDragStart(object sender, PointerDragInteractionEventArgs args)
+        private void OnLeftDragStart(MouseDragEventArgs args)
         {
-            var ray = m_camera.ScreenPointToRay(args.PointerPosition);
+            var ray = m_camera.ScreenPointToRay(args.MousePosition);
             if (m_InteractionPlane.Raycast(ray, out var enter))
             {
                 m_TargetDragStartPoint = TargetPosition;
@@ -67,26 +84,45 @@ namespace Game
             }
         }
 
-        private void OnPointerDrag(object sender, PointerDragInteractionEventArgs args)
+        private void OnLeftDragPerformed(MouseDragEventArgs args)
         {
-            if (m_cameraRaycaster.RaycastDeltaOnPlane(args.PointerPrevPosition, args.PointerPosition, m_InteractionPlane, out var detla))
+            if (!CheckInputInterruption())
             {
-                m_TotalDelta += detla;
-                TargetPosition = m_TargetDragStartPoint - m_TotalDelta;
+                if (m_cameraRaycaster.RaycastDeltaOnPlane(args.MousePosition - args.MouseDelta, args.MousePosition, m_InteractionPlane, out var delta))
+                {
+                    m_TotalDelta += delta;
+                    TargetPosition = m_TargetDragStartPoint - m_TotalDelta;
+                }
             }
         }
 
-        private void OnPointerDragEnd(object sencer, PointerDragInteractionEventArgs args)
+        private void OnLeftDragEnd(MouseDragEventArgs args)
         {
         }
-        
+
+        private void OnRightDragStart(MouseDragEventArgs args)
+        {
+        }
+
+        private void OnRightDragPerformed(MouseDragEventArgs args)
+        {
+            if (!CheckInputInterruption())
+                m_TargetTransform.rotation *= Quaternion.AngleAxis(-args.MouseDelta.x * m_RotationSensetivity, Vector3.up);
+        }
+
+        private void OnRightDragEnd(MouseDragEventArgs args)
+        {
+        }
+
         private void OnWheel(float delta)
         {
-            //We can scroll if no object is under interaction. not a good way to do like this :(
-            var inputManager = InputManager.Instance;
-            var canScroll = inputManager.ActiveGestures.All(c => c.InteractionObject == m_InteractionObject);
-            if (canScroll)
-                Distance += delta * m_WheelSensitive;
+            if (!CheckInputInterruption())
+                Zoom += delta * m_WheelSensitive;
+        }
+
+        private bool CheckInputInterruption()
+        {
+            return InputManager.Instance.ActiveGestures.Any(c => c.InteractionObject != m_InteractionObject);
         }
     }
 }
