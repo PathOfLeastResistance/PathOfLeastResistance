27fa57e7303e35660db25fac552cc391c3804ac9
ViJ <propellerheadvij@gmail.com>
2022-11-27 20:12:39 +0300

    WaveForm ico

--- Assets/Game/PuzzleComponents/WaveFormIco.cs
+++ Assets/Game/PuzzleComponents/WaveFormIco.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using CircuitJSharp;
 using UnityEngine;
 
 public class WaveFormIco : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private VoltageComponent m_voltageComponent;
+
+    [SerializeField] private GameObject m_icoDC;
+    [SerializeField] private GameObject m_icoAC;
+
+    private void Awake()
     {
-        
+        m_voltageComponent = GetComponentInParent<VoltageComponent>();
+        if (m_voltageComponent != null)
+        {
+            m_voltageComponent.SubscribeWaveFormChanged(OnWaveFormChanged);
+        }
+        else
+        {
+            Debug.LogWarning("Voltage component was not found", gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnWaveFormChanged(WaveForm wf)
     {
-        
+        m_icoDC.SetActive(wf == WaveForm.WF_DC);
+        m_icoAC.SetActive(wf == WaveForm.WF_AC);
     }
 }
