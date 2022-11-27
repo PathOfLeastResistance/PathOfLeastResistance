842df0547f1de3fc859b9e705b15f4d8c497a699
ViJ <propellerheadvij@gmail.com>
2022-11-27 20:19:42 +0300

    Added scheme validation to make sure pins are ready for connections

--- Assets/Game/PuzzleComponents/TestSchemeBuilder.cs
+++ Assets/Game/PuzzleComponents/TestSchemeBuilder.cs
@@ -13,22 +13,40 @@ public struct PinPair
     public ConnectorPinBehaviour Pin2;
 }
 
+/// <summary>
+/// This class takes pins that you save in the inspector and connects them together.
+/// </summary>
 public class TestSchemeBuilder : DisposableMonobehaviour
 {
     [Inject] private CableBehaviour.Factory m_cablesFactory;
 
     [SerializeField] private Transform m_connectorsRoot;
     [SerializeField] private List<PinPair> m_pinPairs = new List<PinPair>();
+    
+    private List<CableBehaviour> m_cables = new List<CableBehaviour>();
 
     public void CreateConnections()
     {
         foreach (var pair in m_pinPairs)
         {
+            if (!pair.Pin1.HasId)
+                Debug.LogError($"Pin {pair.Pin1.name} has no id", pair.Pin1);
+            if (!pair.Pin2.HasId)
+                Debug.LogError($"Pin {pair.Pin2.name} has no id", pair.Pin2);
+
             var cable = m_cablesFactory.Create();
             cable.CableEnding1.Pin = pair.Pin1;
             cable.CableEnding2.Pin = pair.Pin2;
             cable.transform.SetParent(m_connectorsRoot);
         }
+    }
+
+    protected override void OnDispose()
+    {   
+        base.OnDispose();
+        foreach (var cable in m_cables)
+            cable.Dispose();
+        m_cables.Clear();
     }
 
     private void OnDrawGizmos()
